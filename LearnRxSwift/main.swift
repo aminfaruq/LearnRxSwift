07b738a7fd0492b9c3ecdab5dad11b2589c6289a
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-23 15:40:30 +0800

    Adds DisposeBag example
    Illustrates how DisposeBag simplifies resource management by automatically disposing of subscriptions when the DisposeBag is deallocated.

--- LearnRxSwift/main.swift
+++ LearnRxSwift/main.swift
@@ -98,3 +98,15 @@ example(of: "dispose") {
     
     subscription.dispose()
 }
+
+example(of: "DisposeBag") {
+    // 1
+    let disposeBag = DisposeBag()
+    
+    // 2
+    Observable.of("A", "B", "C")
+        .subscribe { // 3
+            print($0)
+        }
+        .disposed(by: disposeBag) // 4
+}
