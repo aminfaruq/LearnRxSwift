fcb0ad938dcf2cfa0fddc11b054e9e0964da6961
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-23 14:52:21 +0800

    first commit

---
+++ LearnRxSwift/SupportCode.swift
@@ -0,0 +1,14 @@
+//
+//  SupportCode.swift
+//  LearnRxSwift
+//
+//  Created by Amin faruq on 23/06/25.
+//
+
+import Foundation
+
+public func example(of description: String,
+                      action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
---
+++ LearnRxSwift/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  LearnRxSwift
+//
+//  Created by Amin faruq on 23/06/25.
+//
+
+import RxSwift
