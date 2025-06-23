a68cdf83a2c6bb7e83f99c8585af0fe122946a3a
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-23 14:53:36 +0800

    Adds example of creating Observables
    Demonstrates different ways to create Observables
    using just, of, and from operators.
    
    The example illustrates how to create Observables
    from single values, a list of values, and an array
    of values.

--- LearnRxSwift/main.swift
+++ LearnRxSwift/main.swift
@@ -6,3 +6,16 @@
 //
 
 import RxSwift
+
+example(of: "just, of, from") {
+    // 1
+    let one = 1
+    let two = 2
+    let three = 3
+
+    // 2
+    let observable = Observable<Int>.just(one)
+    let observable2 = Observable.of(one, two, three)
+    let observable3 = Observable.of([one, two, three])
+    let observable4 = Observable.from([one, two, three])
+}
