bf97b7ceff50bb1e843f0e3b4bfbbaaaa8e21fe3
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-23 15:06:15 +0800

    Adds example observable with empty event
    Introduces a new example demonstrating the usage of an
    `Observable` that emits an `empty` event, showcasing how
    to handle the completion of an observable sequence.

--- LearnRxSwift/main.swift
+++ LearnRxSwift/main.swift
@@ -37,3 +37,18 @@ example(of: "subscribe") {
         print(element)
     })
 }
+
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable.subscribe(
+        // 1
+        onNext: { element in
+            print(element)
+        },
+        // 2
+        onCompleted: {
+            print("Completed")
+        }
+    )
+}
