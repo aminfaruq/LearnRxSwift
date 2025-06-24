379e76540b5f57943879f883682954f327aad371
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-24 11:27:08 +0800

    Demonstrates Subject behavior in RxSwift
    Adds examples illustrating subject usage, including multiple
    subscriptions, disposal, and handling completion events.
    
    This enhances understanding of subject lifecycle and its
    interactions with observers.

--- LearnRxSwift/Materials/2 Subject/SubjectsCommand.swift
+++ LearnRxSwift/Materials/2 Subject/SubjectsCommand.swift
@@ -21,6 +21,39 @@ struct SubjectsCommand: Runnable {
                 })
             
             subject.on(.next("1"))
+            subject.on(.next("2"))
+            
+            let subscriptionTwo = subject
+                .subscribe { event in
+                    print("2)", event.element ?? event)
+                }
+            
+            subject.onNext("3")
+            
+            subscriptionOne.dispose()
+            
+            subject.onNext("4")
+            
+            // Add a completed event onto the subject, using the convenience method for on(.completed) . This terminates the subject’s observable sequence.
+            subject.onCompleted()
+            
+            // Add another element onto the subject. This won’t be emitted and printed, though, because the subject has already terminated
+            subject.onNext("5")
+            
+            // Dispose of the subscription.
+            subscriptionTwo.dispose()
+            
+            let disposeBag = DisposeBag()
+            
+            // Subscribe to the subject, this time adding its disposable to a dispose bag.
+            subject
+                .subscribe {
+                    print("3)", $0.element ?? $0)
+                }
+                .disposed(by: disposeBag)
+            
+            subject.onNext("?")
+            
         }
         
     }
