80cf3f3b60704e548baf348e967a0d239599d5dd
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-24 16:05:30 +0800

    Demonstrates BehaviorRelay usage for user sessions
    Illustrates how to use BehaviorRelay to manage user session state.
    
    It showcases login and logout functionalities, and how to perform
    actions based on the current session state, providing a practical
    example of BehaviorRelay in a real-world scenario.

--- LearnRxSwift/Materials/2 Subject/SubjectsChallenges.swift
+++ LearnRxSwift/Materials/2 Subject/SubjectsChallenges.swift
@@ -56,5 +56,70 @@ struct SubjectsChallenges: Runnable {
             
             deal(3)
         }
+        
+        example(of: "BehaviorRelay") {
+            enum UserSession {
+                case loggedIn, loggedOut
+            }
+            
+            enum LoginError: Error {
+                case invalidCredentials
+            }
+            
+            let disposeBag = DisposeBag()
+            
+            // Create userSession BehaviorRelay of type UserSession with initial value of .loggedOut
+            let userSession = BehaviorRelay(value: UserSession.loggedOut)
+            
+            // Subscribe to receive next events from userSession
+            userSession
+                .subscribe(onNext: { sessionState in
+                    print("User status changed to: \(sessionState)")
+                })
+                .disposed(by: disposeBag)
+            
+            func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+                guard username == "johnny@appleseed.com",
+                      password == "appleseed" else {
+                    completion(LoginError.invalidCredentials)
+                    return
+                }
+                
+                // Update userSession
+                userSession.accept(.loggedIn)
+                
+            }
+            
+            func logOut() {
+                // Update userSession
+                userSession.accept(.loggedOut)
+                
+            }
+            
+            func performActionRequiringLoggedInUser(_ action: () -> Void) {
+                // Ensure that userSession is loggedIn and then execute action()
+                if userSession.value == .loggedIn {
+                    action()
+                }
+                
+            }
+            
+            for i in 1...2 {
+                let password = i % 2 == 0 ? "appleseed" : "password"
+                
+                logInWith(username: "johnny@appleseed.com", password: password) { error in
+                    guard error == nil else {
+                        print(error!)
+                        return
+                    }
+                    
+                    print("User logged in.")
+                }
+                
+                performActionRequiringLoggedInUser {
+                    print("Successfully did something only a logged in user can do.")
+                }
+            }
+        }
     }
 }
