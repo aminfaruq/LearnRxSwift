7cf4d1c5dbca5d85eaa179617c825235d77ab21b
Amin Faruq <aminfaruk.fa@gmail.com>
2025-06-30 15:44:21 +0800

    feat: handle error message

--- Example App/Combinestagram/PhotosViewController.swift
+++ Example App/Combinestagram/PhotosViewController.swift
@@ -45,6 +45,7 @@ class PhotosViewController: UICollectionViewController {
   // MARK: private properties
   private lazy var photos = PhotosViewController.loadPhotos()
   private lazy var imageManager = PHCachingImageManager()
+  private let bag = DisposeBag()
 
   private lazy var thumbnailSize: CGSize = {
     let cellSize = (self.collectionViewLayout as! UICollectionViewFlowLayout).itemSize
@@ -61,7 +62,39 @@ class PhotosViewController: UICollectionViewController {
   // MARK: View Controller
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    let authorized = PHPhotoLibrary.authorized
+      .share()
+    
+    authorized
+      .skipWhile({ !$0 })
+      .take(1)
+      .subscribe(onNext: { [weak self] _ in
+        self?.photos = PhotosViewController.loadPhotos()
+        DispatchQueue.main.async {
+          self?.collectionView.reloadData()
+        }
+      })
+      .disposed(by: bag)
+    
+    authorized
+      .skip(1)
+      .takeLast(1)
+      .filter({ !$0 })
+      .subscribe(onNext: { [weak self] _ in
+        guard let errorMessage = self?.errorMessage else { return }
+        DispatchQueue.main.async(execute: errorMessage)
+      })
+      .disposed(by: bag)
+  }
+  
+  private func errorMessage() {
+    showAlert("No access to Camera Roll", description: "You can grant access to Combinestagram from the Settings app")
+      .subscribe(onCompleted: { [weak self] in
+        self?.dismiss(animated: true, completion: nil)
+        _ = self?.navigationController?.popViewController(animated: true)
+      })
+      .disposed(by: bag)
   }
 
   override func viewWillDisappear(_ animated: Bool) {
